db37598330c40b217cb16a502f11ee1ea8aaefdb
matheus-fls <matheus_fls@yahoo.com.br>
2019-09-19 13:49:28 -0300

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
